d5d694f96e1d61d1e712a590561b12ad3327e64c
jeremy289 <jeremy289@gmail.com>
2022-10-04 21:36:15 +0800

    Initial Commit

---
+++ HW2_00857126/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW2_00857126
+//
+//  Created by User18 on 2022/10/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HW2_00857126/HW2_00857126App.swift
@@ -0,0 +1,17 @@
+//
+//  HW2_00857126App.swift
+//  HW2_00857126
+//
+//  Created by User18 on 2022/10/4.
+//
+
+import SwiftUI
+
+@main
+struct HW2_00857126App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
